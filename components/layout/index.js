c0772c21a56390654304376e873f20989430d25f
aldrin v <aldrin.v@sw-lp08548.bundl.in>
2019-08-21 12:26:23 +0530

    Added github corner

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,18 +1,20 @@
 import Header from "../header";
 import Menu from "../menu";
+import GithubCorner from "../github-corner";
 
 const Layout = props => (
-  <main >
+  <main>
     <Header />
     <section className="container">
+      <GithubCorner />
       <Menu />
       {props.children}
     </section>
     <style jsx>{`
-        .container {
-          display: flex;
-        };
-      `}</style>
+      .container {
+        display: flex;
+      }
+    `}</style>
   </main>
 );
 

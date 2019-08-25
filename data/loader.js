df484d82acc95b9e621d01e7c7f70e7874bbcf1e
aldrin v <aldrin.v@SW-LP08548.local>
2019-08-25 14:37:00 +0530

    dot loader

--- data/loader.js
+++ data/loader.js
@@ -59,5 +59,71 @@ div {
   }
 }
 `
+  },
+  {
+    name: "Dot-loader",
+    html: `<div class="dot-loader">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>`,
+    css: `
+.dot-loader {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+}
+.dot-loader div {
+  position: absolute;
+  top: 27px;
+  width: 11px;
+  height: 11px;
+  border-radius: 50%;
+  background: #fa7d16;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.dot-loader div:nth-child(1) {
+  left: 6px;
+  animation: dot1 0.6s infinite;
+}
+.dot-loader div:nth-child(2) {
+  left: 6px;
+  animation: dot2 0.6s infinite;
+}
+.dot-loader div:nth-child(3) {
+  left: 26px;
+  animation: dot2 0.6s infinite;
+}
+.dot-loader div:nth-child(4) {
+  left: 45px;
+  animation: dot3 0.6s infinite;
+}
+@keyframes dot1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes dot2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(19px, 0);
+  }
+}
+@keyframes dot3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+    `
   }
 ]
